31029078b048c1d504da8cc16ad2f7b6f71c0e03
mateorou <mateo.roura@campus.xaloc.org>
2026-09-18 09:26:29 +0200

    Add files via upload

---
+++ 7operaciones_170926.py
@@ -0,0 +1,13 @@
+#enunciado
+#el usuario introduce dos valores y los convierto a numéricos
+
+variable1=int(input("Introduce el primer número: "))
+variable2=int(input("Introduce el segundo número: "))
+
+print ("La suma es:",variable1+variable2)
+print ("La resta es:",variable1-variable2)
+print ("La multiplicación es:",variable1*variable2)
+print ("La división es:",variable1/variable2)
+print ("La módulo es:",variable1%variable2)
+print ("La potencia es:",variable1**variable2)
+print ("La módulo entera es:",variable1%variable2)
